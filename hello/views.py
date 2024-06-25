7493e17ef092aafbedcdb94de1227923bdd8ba82
ana.rebli <aninharebli@gmail.com>
2024-06-25 09:54:22 -0300

    voltar ao ajuste normal

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, render
 
 def home(request):
-    #return HttpResponse("Olá, mundo!! <br> A página de Django, da Ana Rebli, está rodando!!")
-    return render(request, 'index.html')
+    return HttpResponse("Olá, mundo!! <br> A página de Django, da <b>Ana Rebli</b>, para a matéria de Computação em Núvem, está rodando!!")
+    #return render(request, 'index.html')
