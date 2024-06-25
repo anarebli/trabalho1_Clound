1addf211a101105355fd06a9231d0a597902462f
ana.rebli <aninharebli@gmail.com>
2024-06-25 10:01:58 -0300

    Revert "Ajuste nome"
    This reverts commit d4b300fca502995ead7dfe28a860ec85b34263e0.

--- trabalho1_clound/urls.py
+++ trabalho1_clound/urls.py
@@ -1,4 +1,4 @@
-"""trabalho1_clound URL Configuration
+"""web_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
